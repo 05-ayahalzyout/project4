9e1f7ae82449a4fb14171cbe453dc3eca0e52e6d
05-ayahalzyout <115452706+05-ayahalzyout@users.noreply.github.com>
2023-02-26 12:20:45 +0200

    project4

---
+++ Survey-Project-main/C sharp project/C sharp project/addQuestions.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace C_sharp_project
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+
+            string[] questions = String.Join(",", Form.Controls.OfType<TextBox>().Select(c => c.Text)).Split(',');
+
+            
+          
+
+
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
